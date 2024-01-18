50073550b4fc10f7ffe11eda470a2a631c8f220f
Lon Beshiri <lonbeshiri@gmail.com>
2024-01-18 16:30:59 -0500

    feat: FileSelect update

--- src/components/FileSelect.tsx
+++ src/components/FileSelect.tsx
@@ -1,11 +1,23 @@
+import { CSSProperties, useRef } from 'react'
+import { useDarkMode } from 'usehooks-ts'
+
 export type FileSelectProps = {
   name?: string
   label?: string
   className?: string
   classNames?: {
+    wrapper?: string
     label?: string
     input?: string
   }
+  style?: CSSProperties
+  styles?: {
+    wrapper?: CSSProperties
+    label?: CSSProperties
+    input?: CSSProperties
+  }
+  styleNamespace?: string
+  dark?: boolean
   placeholder?: string
   isDisabled?: boolean
   setValue?: (file: File) => void
@@ -16,34 +28,75 @@ export default function FileSelect({
   label,
   className,
   classNames,
+  style,
+  styles,
+  styleNamespace = 'global',
+  dark,
   placeholder,
   isDisabled,
   setValue = () => {},
 }: FileSelectProps) {
+  const { isDarkMode } = useDarkMode()
+
+  const isDark = dark || isDarkMode
+
+  const namespaceColors = {
+    darker: !isDark
+      ? `var(--${styleNamespace}-darker)`
+      : `var(--${styleNamespace}-lighter)`,
+    lighter: !isDark
+      ? `var(--${styleNamespace}-lighter)`
+      : `var(--${styleNamespace}-darker)`,
+  }
+
+  const colors = {
+    ...namespaceColors,
+    background: namespaceColors.lighter,
+    text: namespaceColors.darker,
+    border: namespaceColors.darker,
+  }
+
+  const inputElement = useRef<HTMLInputElement>(null)
+
+  if (inputElement.current)
+    inputElement.current.style.setProperty(
+      '--placeholder-color',
+      !dark ? namespaceColors.darker : namespaceColors.lighter
+    )
   return (
-    <div className={className}>
+    <div className={className} style={style}>
       <div
-        className="flex flex-col bg-global-flipped rounded-xl px-2 py-1 border-global-flipped border-2"
-        style={{ opacity: isDisabled ? 0.5 : 1 }}
+        className={
+          classNames?.wrapper || 'flex flex-col rounded-xl px-2 py-1 border-2'
+        }
+        style={{
+          ...(styles?.wrapper || {
+            backgroundColor: colors.background,
+            color: colors.text,
+            borderColor: colors.border,
+          }),
+          opacity: isDisabled ? 0.5 : 1,
+        }}
       >
         {label && (
-          <span
-            className={
-              classNames?.label || 'text-global-flipped font-header text-sm'
-            }
+          <label
+            className={classNames?.label || 'font-header text-sm'}
+            style={styles?.label}
+            htmlFor={name}
           >
             {label}
-          </span>
+          </label>
         )}
         <input
           type="file"
           name={name}
           className={
             classNames?.input ||
-            'cursor-pointer text-global-flipped font-content file:rounded-xl file:border-solid file:border-2 file:border-global-flipped file:bg-global-flipped file:text-global-flipped file:text-xs file:font-content file:mr-2'
+            'cursor-pointer text-global-flipped font-content file:rounded-xl file:border-solid file:border-2 file:border-global file:bg-global-flipped file:text-global-flipped file:text-xs file:font-content file:mr-2'
           }
           placeholder={placeholder}
           onChange={(e) => setValue(e.target.files![0])}
+          ref={inputElement}
         />
       </div>
     </div>
